33a1f7beee46885a0feecfd555232b6a0eb274f2
aashishChakradhar <ashishchakradhar01@gmail.com>
2024-08-11 20:26:18 +0545

    dictionary modification

--- dictionary.py
+++ dictionary.py
@@ -2,6 +2,28 @@ mydict = {
     'one':'hello',
     'two':'world',
 }
+print(f'type of :{type(mydict)}')
 print(f'keys: {mydict.keys()}')
 print(f'values: {mydict.values()}')
+print(f'dictionary: {mydict}')
 print(f'items: {mydict.items()}')
+
+# modifying using list
+listKeys = ['apple','ball','cat']
+listValues = ['fruit','object','animal']
+newDict = dict(zip(listKeys,listValues))
+print(f'{newDict}')
+
+#update in dictionary
+newDict.update({'hello':'world'})
+print(f'{newDict}')
+
+# #task quiz question answer using zip 
+# question = []
+# answer = []
+# num = int(input('Enter number of question: '))
+# for i in range(num):
+#     question.append(input(f"question {i+1}: "))
+#     answer.append(input(f"answer {i+1}: "))
+# questionDictionary = dict(zip(question,answer))
+# print(f'{questionDictionary}')
