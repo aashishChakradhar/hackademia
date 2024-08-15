7ba9c22b11e4503b8ab839a6a180b82fd258d583
aashishChakradhar <ashishchakradhar01@gmail.com>
2024-08-15 19:20:44 +0545

    filter and map in python

---
+++ map/basic.py
@@ -0,0 +1,68 @@
+'''
+    map takes in function and collection 
+    and iterates all elements one by one 
+    over that function
+    - map can also be used to map out 2
+    different parameters from 2 different 
+    collections over a function
+'''
+# print('--'*25)
+# list_of_numbers = [i for i in range(10)]
+# output_of_map = list(map(lambda x: x**2,list_of_numbers))
+# # syntax map(function,list/iterable)
+# print(output_of_map)
+
+# print('--'*25)
+# def add_square(x,y):
+#     return(x**2 + y**2)
+# list_A = [i for i in range(1,6)]
+# list_B = [i for i in range(6,11)]
+# print(list_A + list_B)
+# output_of_map_2 = list(map(add_square,list_A,list_B))
+# print(output_of_map_2)
+
+# '''
+#     task1
+#     read two list from user
+#     if same element of two list is 10 then concatinate them
+#     eg: 1+9 = 19
+# '''
+# def conList (x,y):
+#     if x + y == 10:
+#         return str(x) + str(y)
+
+
+
+# N = int(input('Enter number of element to input: '))
+# listA = [ int(input(f'For List 1 element {1+i}: ')) for i in range(N)]
+# listB = [ int(input(f'For List 2 element {1+i}: ')) for i in range(N)]
+
+# output_of_map = list(map(conList,listA,listB))
+# print(output_of_map)
+
+
+'''
+    Filter:
+        filter works very similar to map
+        it takes function and iterable as input
+        returns output whenever it is true
+'''
+# print('=='*25)
+# numbers = [i for i in range (10)]
+# evenMap = list(map(lambda x: x%2==0,numbers))
+# evens = list(filter(lambda x: x%2 == 0, numbers))
+# print(evenMap)
+# print(evens)
+
+# '''
+#     given a listA create another list containig elements with 
+#     length more than 3
+# '''
+# listA = ['apple','ball','cat','dog','elephant']
+# outputList = list(filter(lambda x: len(x)>3,listA))
+# print(outputList)
+
+'''
+
+'''
+print('--'*25)
